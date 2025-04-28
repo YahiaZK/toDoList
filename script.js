8ced59e45d8c9d6199d5a6c8e68040f915541626
yahya <yahiamohammad740@gmail.com>
2025-04-28 20:39:37 +0300

    final version

--- script.js
+++ script.js
@@ -109,12 +109,13 @@ const createOneTask = (taskObject) => {
               </label>
               <div class="taskIcons">
                 <i class="fa-solid fa-pen edit-btn"></i>
-                <i class="fa-solid fa-trash"></i>
+                <i class="fa-solid fa-trash delete-btn"></i>
               </div>
   `;
 
   changeCheckbox(taskElement, taskObject);
   renameTask(taskElement, taskObject);
+  deleteTask(taskElement, taskObject);
 
   return taskElement;
 };
@@ -179,6 +180,16 @@ const renameTask = (taskElement, taskObject) => {
   });
 };
 
+const deleteTask = (taskElement, taskObject) => {
+  const deleteBtn = taskElement.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    allTasksArray = allTasksArray.filter((task) => task.id !== taskObject.id);
+    saveTasks();
+    createTasksList();
+  });
+};
+
 const showPopupEdit = () => {
   const popupEdit = document.getElementById("popupEdit");
   popupEdit.style.display = "flex";
@@ -207,138 +218,65 @@ const getTasks = () => {
 allTasksArray = getTasks();
 createTasksList();
 
-function showPopup(popupTitle, popupText) {
-  const popup = document.querySelector(".popup");
-  popup.style.display = "flex";
-  document.getElementById("popupText").innerText = popupText;
-  document.getElementById("popupTitle").innerText = popupTitle;
-}
-function hidePopup() {
-  const popup = document.querySelector(".popup");
-  popup.style.display = "none";
-}
+const deleteAllBtn = document.getElementById("deleteAllBtn");
+const deleteDoneBtn = document.getElementById("deleteDoneBtn");
+const popupDeleteAllCancelBtn = document.getElementById(
+  "popupDeleteAllCancelBtn"
+);
+const popupDeleteAllConfirmBtn = document.getElementById(
+  "popupDeleteAllConfirmBtn"
+);
+const popupDeleteDoneCancelBtn = document.getElementById(
+  "popupDeleteDoneCancelBtn"
+);
+const popupDeleteDoneConfirmBtn = document.getElementById(
+  "popupDeleteDoneConfirmBtn"
+);
 
-document.addEventListener("DOMContentLoaded", () => {
-  const tasksList = document.getElementById("tasksList");
-  const deleteAllBtn = document.getElementById("deleteAllBtn");
-  const deleteDoneBtn = document.getElementById("deleteDoneBtn");
-  const popup = document.getElementById("popup");
-  const allTasks = document.querySelectorAll(".task");
-  let doneTasksCheckbox = [];
-  let doneTasks = [];
-  let confirmDelete = null;
-  let taskToDelete = null;
-
-  updateDeleteAllBtnState();
-  updateDoneTasksCheckbox();
-  updateDoneTasksArr();
-  updateDeleteDoneBtnState();
-
-  tasksList.addEventListener("click", (e) => {
-    const isDeleteBtn = e.target.id.startsWith("deleteBtn-");
-
-    if (isDeleteBtn) {
-      const taskId = e.target.id.replace("deleteBtn-", "task-");
-      taskToDelete = document.getElementById(taskId);
-      if (taskToDelete) {
-        const taskText =
-          taskToDelete.querySelector(".taskText")?.textContent || "this task";
-        showPopup(
-          "Delete Task",
-          `Are you sure you want yo delete ${taskText} ?`
-        );
-        confirmDelete = "single";
-      }
-    }
-  });
-
-  tasksList.addEventListener("change", (e) => {
-    if (e.target.type === "checkbox") {
-      console.log("True");
-      updateDoneTasksCheckbox();
-      updateDoneTasksArr();
-      updateDeleteDoneBtnState();
-    }
-  });
-
-  deleteAllBtn.addEventListener("click", () => {
-    showPopup("Delete All Tasks", "Are you sure you want to delete all tasks?");
-    confirmDelete = "all";
-  });
-
-  deleteDoneBtn.addEventListener("click", () => {
-    showPopup(
-      "Delete Done Tasks",
-      "Are you sure you want to delete all done tasks?"
-    );
-    confirmDelete = "doneTasks";
-  });
-
-  popup.addEventListener("click", (e) => {
-    const isConfirm = e.target.id === "popupConfirmBtn";
-    const isCancel = e.target.id === "popupCancelBtn";
-
-    if (isConfirm) {
-      if (confirmDelete === "single" && taskToDelete) {
-        taskToDelete.remove();
-      } else if (confirmDelete === "all") {
-        allTasks.forEach((task) => {
-          task.remove();
-        });
-      } else if (confirmDelete === "doneTasks") {
-        updateDoneTasksCheckbox();
-        console.log(doneTasksCheckbox);
-        doneTasksCheckbox.forEach((el) => {
-          console.log(el);
-          taskId = el.id.replace("taskCheckbox", "task");
-          taskToDelete = document.getElementById(taskId);
-          doneTasks.push(taskToDelete);
-          taskToDelete.remove();
-        });
-      }
-    }
-
-    if (isConfirm && isCancel) {
-      confirmDelete = null;
-      taskToDelete = null;
-    }
-
-    hidePopup();
-    setTimeout(
-      updateDeleteAllBtnState(),
-      updateDoneTasksCheckbox(),
-      updateDoneTasksArr(),
-      updateDeleteDoneBtnState(),
-      0
-    );
-  });
-
-  function updateDeleteAllBtnState() {
-    deleteAllBtn.disabled = tasksList.children.length === 0;
-  }
-
-  function updateDeleteDoneBtnState() {
-    deleteDoneBtn.disabled = doneTasks.length === 0;
-    console.log(doneTasks.length);
-    console.log(doneTasks);
-  }
-
-  function updateDoneTasksArr() {
-    let task;
-    let taskId;
-    doneTasks = [];
-    doneTasksCheckbox.forEach((checkbox) => {
-      taskId = checkbox.id.replace("taskCheckbox", "task");
-      task = document.getElementById(taskId);
-      doneTasks.push(task);
-    });
-    console.log(doneTasks);
-  }
-
-  function updateDoneTasksCheckbox() {
-    doneTasksCheckbox = tasksList.querySelectorAll(
-      'input[type="checkbox"]:checked'
-    );
-    console.log(doneTasksCheckbox);
-  }
+deleteAllBtn.addEventListener("click", () => {
+  showPopupDeleteAll();
 });
+
+popupDeleteAllConfirmBtn.addEventListener("click", () => {
+  allTasksArray = [];
+  saveTasks();
+  createTasksList();
+  hidePopupDeleteAll();
+});
+popupDeleteAllCancelBtn.addEventListener("click", () => {
+  hidePopupDeleteAll();
+});
+
+const showPopupDeleteAll = (popupText, popupTitle) => {
+  const popup = document.getElementById("popupDeleteAll");
+  popup.style.display = "flex";
+};
+
+const hidePopupDeleteAll = () => {
+  const popup = document.getElementById("popupDeleteAll");
+  popup.style.display = "none";
+};
+
+deleteDoneBtn.addEventListener("click", () => {
+  showPopupDeleteDone();
+});
+
+popupDeleteDoneConfirmBtn.addEventListener("click", () => {
+  allTasksArray = allTasksArray.filter((taskObject) => !taskObject.completed);
+  saveTasks();
+  createTasksList();
+  hidePopupDeleteDone();
+});
+popupDeleteDoneCancelBtn.addEventListener("click", () => {
+  hidePopupDeleteDone();
+});
+
+const showPopupDeleteDone = (popupText, popupTitle) => {
+  const popup = document.getElementById("popupDeleteDone");
+  popup.style.display = "flex";
+};
+
+const hidePopupDeleteDone = () => {
+  const popup = document.getElementById("popupDeleteDone");
+  popup.style.display = "none";
+};
